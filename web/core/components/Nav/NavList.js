39851a92f7ead0235ddc6524c5de4c703fc79aa5
Italo Devoto Ramella <italodr@gmail.com>
2019-11-06 12:58:31 +0100

    Sass compilation, Minimum framework

---
+++ web/core/components/Nav/NavList.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const NavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  li {
+    margin: 0 0 0.5rem;
+  }
+`;
+
+export default NavList;
