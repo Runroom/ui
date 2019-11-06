89d184209a893f67f6215fe5152412e9884f1035
Italo Devoto Ramella <italodr@gmail.com>
2019-11-06 15:37:23 +0100

    Fix code rendering with javascript Update gitignore

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: false,
+  entry: glob
+    .sync('./packages/ui/components/**/scripts.js')
+    .reduce(
+      (entries, entry) => Object.assign(entries, { [entry.replace('/scripts.js', '')]: entry }),
+      {}
+    ),
+  output: {
+    filename: './[name]/scripts.min.js',
+    path: path.resolve(__dirname)
+  },
+  mode: 'production',
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
