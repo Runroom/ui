22fc2dd81bedfe8108596f3cb59a918b97adfd86
“Raquel <raquel@runroom.com>
2019-12-10 11:49:51 +0100

    Create checkbox component

---
+++ web/pages/checkbox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Code from '../components/Code';
+
+const Checkbox = () => (
+  <>
+    <Head>
+      <title>Component Checkbox</title>
+    </Head>
+
+    <h1>Checkbox component</h1>
+    <Code component='checkbox' />
+  </>
+);
+
+export default Checkbox;
