4ac00595c1e3ce9ea0de1a17054e6cf0c06ea9de
Jose <jose@runroom.com>
2020-02-11 18:30:29 +0100

    Updates to purejs v3

--- packages/ui/components/sample/scripts.js
+++ packages/ui/components/sample/scripts.js
@@ -1,4 +1,4 @@
-import { events } from '@runroom/purejs';
+import events from '@runroom/purejs/lib/events';
 
 events.onDocumentReady(() => {
   document.querySelector('button').addEventListener('click', () => alert('Oh yeah!'));
