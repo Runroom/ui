899aba8888e27347beb98d1e951cbb51bc099db9
O30 <o30@MacBook-Pro-de-O30.local>
2020-02-19 11:04:41 +0100

    Create radio component

---
+++ web/pages/radio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Code from '../components/Code';
+
+const Radio = () => (
+  <>
+    <Head>
+      <title>Component Radio</title>
+    </Head>
+
+    <h1>Radio component</h1>
+    <Code component='radio' />
+  </>
+);
+
+export default Radio;
