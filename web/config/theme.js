7d7b65f1fcfdaad369fa768a9abaa09be5248095
Jose <jose@runroom.com>
2020-03-04 11:41:09 +0100

    Adds news colors

--- web/config/theme.js
+++ web/config/theme.js
@@ -1,8 +1,13 @@
 const theme = {
   colors: {
-    primary100: '#e1e1e1',
-    primary500: '#444',
-    primary900: '#1b1b1b'
+    neutro100: '#f2f2f2',
+    neutro200: '#e8e8e8',
+    neutro300: '#d4d4d4',
+    neutro600: '#b0b0b0',
+    neutro800: '#474747',
+    neutro900: '#191919',
+    primary300: '#ffc1bb',
+    secondary300: '#0032C6'
   }
 };
 
