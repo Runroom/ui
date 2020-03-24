2c886c749032e9124b9c8c01e14ef93e360b1c72
Jose <jose@runroom.com>
2020-03-24 19:44:15 +0100

    Turns video into lazy video

---
+++ packages/ui/components/lazyVideo/script.js
@@ -0,0 +1,31 @@
+import forEach from '@runroom/purejs/lib/forEach';
+import intersectionObserver from './helpers/intersectionObserver';
+
+const VIDEO_CLASS = 'lazyload';
+const HANDLED_CLASS = 'lazyloaded';
+const CONFIG = {
+  elementsClass: `.${VIDEO_CLASS}`
+};
+
+const loadVideo = video => {
+  const sources = [].slice.call(video.children);
+
+  forEach(sources, source => {
+    const { src } = source.dataset;
+    source.src = src;
+  });
+
+  video.load();
+
+  video.addEventListener('canplaythrough', () => {
+    video.classList.add(HANDLED_CLASS);
+
+    // if (video.getAttribute('autoplay')) {
+    //   video.play();
+    // }
+  }, false);
+};
+
+intersectionObserver(CONFIG, video => {
+  loadVideo(video);
+});
