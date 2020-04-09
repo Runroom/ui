8ebc88aa7705b3caedacbfaab05676c9d1df9824
Adrià Marcet <amarcet@runroom.com>
2020-04-09 13:13:33 +0200

    Merge ifs into one

--- packages/ui/helpers/toggle/index.js
+++ packages/ui/helpers/toggle/index.js
@@ -10,19 +10,23 @@ const toggleVisibility = element => {
 
 const toggleExpanded = element => {
   const attr = element.getAttribute('aria-expanded');
+
   if (!attr.length) return;
   element.setAttribute('aria-expanded', attr === 'true' ? 'false' : 'true');
 };
 
 const toggle = () => {
   if (!toggles.length) return;
+
   forEach(toggles, toggle => {
     toggle.addEventListener('click', () => {
       const target = toggle.getAttribute('aria-controls');
       const targetElement = document.getElementById(target);
-      if (targetElement == null) return;
-      if (toggle.getAttribute('aria-expanded') == null) return;
+
+      if (targetElement === null || toggle.getAttribute('aria-expanded') === null) return;
+
       toggleVisibility(targetElement);
+
       toggleExpanded(toggle);
     });
   });
