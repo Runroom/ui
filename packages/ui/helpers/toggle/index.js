3c0ef0affa1acbd43af571541e1900aa27fb3641
Adrià Marcet <amarcet@runroom.com>
2020-04-09 12:31:26 +0200

    Check if element and attr exist first

--- packages/ui/helpers/toggle/index.js
+++ packages/ui/helpers/toggle/index.js
@@ -3,31 +3,27 @@ import forEach from '@runroom/purejs/lib/foreach'
 const TOGGLE_CLASS = 'js-toggle'
 const toggles = document.querySelectorAll(`.${TOGGLE_CLASS}`);
 
-const handleVisibility = element => {
+const toggleVisibility = element => {
   if (element === 'undefined' && !element.length) return;
   return element.hidden = !element.hidden;
 };
 
-const handleExpanded = element => {
+const toggleExpanded = element => {
   const attr = element.getAttribute('aria-expanded');
-
   if (!attr.length) return;
   element.setAttribute('aria-expanded', attr === 'true' ? 'false' : 'true');
 };
 
 const toggle = () => {
   if (!toggles.length) return;
-
   forEach(toggles, toggle => {
     toggle.addEventListener('click', () => {
       const target = toggle.getAttribute('aria-controls');
       const targetElement = document.getElementById(target);
-
       if (targetElement == null) return;
       if (toggle.getAttribute('aria-expanded') == null) return;
-
-      handleVisibility(targetElement);
-      handleExpanded(targetElement);
+      toggleVisibility(targetElement);
+      toggleExpanded(toggle);
     });
   });
 };
