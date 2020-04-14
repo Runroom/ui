c04ab0325521e40a525f708b3934179568b79f68
rocio-m-sebastian <56320085+rocio-m-sebastian@users.noreply.github.com>
2020-04-14 13:52:08 +0200

    Feature/skip links (#14)
    * skip-links component

---
+++ web/pages/skip-links.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+import Code from '../components/Code';
+
+const Skiplinks = () => (
+  <>
+    <Head>
+      <title>Component Skip-links</title>
+    </Head>
+
+    <h1>Skip-links component</h1>
+    <Code component='skip-links' />
+  </>
+);
+
+export default Skiplinks;
