f1090c923305e894f7d03f6c1a1903166184baf2
Italo Devoto Ramella <italodr@gmail.com>
2020-04-23 12:24:05 +0200

    Update project to be deploy on Netlify (#46)
    - Update dependencies, Add resolutions to minimist version
    - Reestructuring for dynamic routes
    - Update project to be deployed on Netlify
    - Optimize components page with dynamic route
    - Move server to root
    - Remove interface from project
    - Update yarn.lock

--- api/routes/ui.js
+++ api/routes/ui.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 const router = express.Router();
-const { TwingEnvironment, TwingLoaderFilesystem } = require('twing');
+const { TwingEnvironment, TwingLoaderFilesystem } = require("twing");
 
 const UI_PATH = `${__dirname}/../../packages/ui`;
 const loader = new TwingLoaderFilesystem(UI_PATH);
@@ -10,7 +10,7 @@ const twing = new TwingEnvironment(loader);
 // @route   GET api/ui
 // @desc    Get component by directory name
 // @access  Public
-router.get('/:component', async (req, res) => {
+router.get("/:component", async (req, res) => {
   const component = req.params.component;
   const stylesFilePath = `${UI_PATH}/components/${component}/styles.css`;
   const scriptsFilePath = `${UI_PATH}/components/${component}/scripts.min.js`;
@@ -33,10 +33,10 @@ router.get('/:component', async (req, res) => {
   }
 
   try {
-    const output = await twing.render('/layouts/sandbox.twig', {
+    const output = await twing.render("/layouts/sandbox.twig", {
       component,
       styles,
-      scripts
+      scripts,
     });
 
     fs.writeFile(`${__dirname}/../public/${component}.html`, output, (err) => {
