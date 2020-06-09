fe9c986b7aee931c74d236b4fe28ef3915ea4cf2
Jose <jose@runroom.com>
2020-06-09 11:27:43 +0200

    Returns json structure as lambda function

--- express/server.js
+++ express/server.js
@@ -4,6 +4,26 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const whitelist = [
+  "http://127.0.0.1:3000",
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "http://localhost:5000",
+  "https://localhost:5000",
+  "https://runroom-ui-app.netlify.app",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -11,12 +31,28 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/list', (req, res) => {
+  const list = require("../api/list.json");
 
+  return res.json(list);
+});
+
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(express.json({ extended: false }));
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use('/list', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use('/list', (req, res) => {
+//   const list = require("../api/list.json");
+//   console.log('List!! ', list);
+//   return list;
+// });
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
