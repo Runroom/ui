6a3c0c21073a5aa689c23d1eafabbbda0e3e25b4
Jose <jose@runroom.com>
2020-07-02 16:01:23 +0200

    Adds cors options to express router

--- server/express.js
+++ server/express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 
 const whitelist = [
   'http://127.0.0.1:3000',
@@ -11,39 +11,25 @@ const whitelist = [
   'https://localhost:3000',
   'https://runroom-ui-app.netlify.app/'
 ];
-const headers = [
-  'Origin',
-  'X-Requested-With',
-  'Content-Type',
-  'Accept'
-];
-
-// const corsOptions = {
-//   // origin: (origin, callback) => {
-//   //   if (!origin || whitelist.indexOf(origin) !== -1) {
-//   //     callback(null, true);
-//   //   } else {
-//   //     callback(new Error('Not allowed by CORS'));
-//   //   }
-//   // },
-//   origin: whitelist,
-//   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-//   optionsSuccessStatus: 200
-// };
+const corsOptions = {
+  origin: whitelist,
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  optionsSuccessStatus: 200
+};
 
 const router = express.Router();
-router.get('/', (req, res) => {
+router.get('/', cors(corsOptions), (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from UI!</h1>');
   res.end();
 });
-router.get('/list', (req, res) => res.json(require('./list.json')));
+router.get('/list', cors(corsOptions), (req, res) => res.json(require('./list.json')));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', whitelist.join(', '));
-  res.header('Access-Control-Allow-Headers', headers.join(', '));
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://runroom-ui-app.netlify.app/');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 app.use(express.static('./public'));
 app.use(express.static('./ui'));
 app.use(express.json({ extended: false }));
