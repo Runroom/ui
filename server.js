8d2561ce2d92834beac8e5384651a557d75f80b1
Jose <jose@runroom.com>
2020-07-02 16:57:33 +0200

    Iterates lambda scripts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = require('./server/express.js');
+const app = require('./express/server.js');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
