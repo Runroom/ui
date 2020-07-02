5d1d74ca5a81ccb1e0afe97acc06835321f279a7
Jose <jose@runroom.com>
2020-07-02 18:32:13 +0200

    Compiles component async and returns promise

--- bin/markup.js
+++ bin/markup.js
@@ -10,50 +10,51 @@ const twing = new TwingEnvironment(loader);
 // @route   GET api/ui
 // @desc    Get component by directory name
 // @access  Public
-const component = name => new Promise((resolve, reject) => {
-  // const component = name;
-  // const stylesFilePath = `${UI_PATH}/components/${component}/styles.css`;
-  // const scriptsFilePath = `${UI_PATH}/components/${component}/scripts.min.js`;
+const component = async (name) => {
+  const component = name;
+  const stylesFilePath = `${UI_PATH}/components/${component}/styles.css`;
+  const scriptsFilePath = `${UI_PATH}/components/${component}/scripts.min.js`;
 
-  // let styles = false;
-  // let scripts = false;
+  let styles = false;
+  let scripts = false;
 
-  // if (fs.existsSync(stylesFilePath)) {
-  //   styles = await fs.readFileSync(stylesFilePath, (err, data) => {
-  //     if (err) return false;
-  //     return data.toString();
-  //   });
-  // }
+  if (await fs.existsSync(stylesFilePath)) {
+    styles = await fs.readFileSync(stylesFilePath, (err, data) => {
+      if (err) return false;
+      return data.toString();
+    });
+  }
 
-  // if (fs.existsSync(scriptsFilePath)) {
-  //   scripts = await fs.readFileSync(scriptsFilePath, (err, data) => {
-  //     if (err) return false;
-  //     return data.toString();
-  //   });
-  // }
+  if (await fs.existsSync(scriptsFilePath)) {
+    scripts = await fs.readFileSync(scriptsFilePath, (err, data) => {
+      if (err) return false;
+      return data.toString();
+    });
+  }
 
-  // try {
-  //   const output = twing.render('/layouts/sandbox.twig', {
-  //     component,
-  //     styles,
-  //     scripts
-  //   });
+  const output = await twing.render('/layouts/sandbox.twig', {
+    component,
+    styles,
+    scripts
+  });
 
-  //   fs.writeFile(`${__dirname}/../public/${component}.html`, output, err => {
-  //     if (err) throw err;
-  //   });
-
-    resolve();
-  // } catch (err) {
-  //   reject();
-  //   throw new Error(err.message);
-  // }
-});
+  return new Promise((resolve, reject) => {
+    fs.writeFile(`${__dirname}/../public/${component}.html`, output, err => {
+      if (err) {
+        reject();
+        throw new Error(err.message);
+      }
+      console.log(`Component: ${component} compiled`);
+      resolve();
+    });
+    console.log('After writefile');
+  });
+};
 
 Promise.all([
   component('button')
 ]).then(() => {
   console.log('Markup files compiled.');
 }).catch(err => {
-  console.err(err);
+  console.error(err);
 });
