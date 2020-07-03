1be6640e4a053d13f986d89789b5042205f7a452
Jose <jose@runroom.com>
2020-07-03 10:09:39 +0200

    Generates images copying promises

--- bin/images.js
+++ bin/images.js
@@ -2,59 +2,37 @@
 
 const fs = require('fs');
 const componentList = require('../express/list.json');
+const { resolve } = require('path');
 const UI_PATH = `${__dirname}/../ui`;
 const PUBLIC_PATH = `${__dirname}/../public`;
 
 const copyImage = async name => {
-  const imagesFilePath = `${UI_PATH}/components/${component}.jpg`;
-  // const scriptsFilePath = `${UI_PATH}/components/${component}/scripts.min.js`;
+  const imagesPath = `${UI_PATH}/components/${name}.jpg`;
+  const imagesNewPath = `${PUBLIC_PATH}/images/${name}.jpg`;
 
-  // let styles = false;
-  // let scripts = false;
-
-  // if (await fs.existsSync(stylesFilePath)) {
-  //   styles = await fs.readFileSync(stylesFilePath, (err, data) => {
-  //     if (err) return false;
-  //     return data.toString();
-  //   });
-  // }
-
-  // if (await fs.existsSync(scriptsFilePath)) {
-  //   scripts = await fs.readFileSync(scriptsFilePath, (err, data) => {
-  //     if (err) return false;
-  //     return data.toString();
-  //   });
-  // }
-
-  // const output = await twing.render('/layouts/sandbox.twig', {
-  //   component,
-  //   styles,
-  //   scripts
-  // });
-
-  // return new Promise((resolve, reject) => {
-  //   fs.writeFile(`${PUBLIC_PATH}/${component}.html`, output, err => {
-  //     if (err) {
-  //       reject();
-  //       throw new Error(err.message);
-  //     }
-  //     resolve();
-  //   });
-  // });
+  return new Promise((resolve, reject) => {
+    fs.copyFile(imagesPath, imagesNewPath, err => {
+      if (err) {
+        reject();
+        throw new Error(err.message);
+      }
+      resolve();
+    });
+  });
 };
 
 const promises = [];
 
 Object.keys(componentList).map(key => {
   componentList[key].map(component => {
-    const { img } = component;
+    const name = component.name.toLowerCase().replace(' ', '-');
     promises.push(copyImage(name));
   });
 });
 
 Promise.all(promises)
   .then(() => {
-    console.log('Markup files compiled.');
+    console.log('Images files copied.');
   }).catch(err => {
     console.error(err);
   });
